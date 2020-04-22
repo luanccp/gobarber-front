d41ab2edfcfc9fb9e54b995b04817276c20b28b7
Luan Castheloge <luanccp@gmail.com>
2020-04-22 13:11:07 -0300

    style: register screen

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
+import { Container, Content, Background } from './styles';
+
+import logoImg from '../../assets/logo.svg';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Background />
+    <Content>
+      <img src={logoImg} alt="Logo GoBarber" />
+      <form>
+        <h1>Faça seu cadastro</h1>
+        <Input name="name" icon={FiUser} placeholder="Nome" />
+        <Input name="email" icon={FiMail} placeholder="Email" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+        <Button type="submit">Cadastrar</Button>
+      </form>
+      <a href="create">
+        <FiArrowLeft size={16} />
+        Voltar para Login
+      </a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;
