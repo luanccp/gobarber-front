5dc5ec2d20df56921a89c2adbf144dc47a582c59
Luan Castheloge <luanccp@gmail.com>
2020-04-22 12:05:27 -0300

    style: transform input and button into a component

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Container, Content, Background } from './styles';
 
 import logoImg from '../../assets/logo.svg';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 const SignIn: React.FC = () => (
   <Container>
@@ -10,9 +12,14 @@ const SignIn: React.FC = () => (
       <img src={logoImg} alt="Logo GoBarber" />
       <form>
         <h1>Faça seu logon</h1>
-        <input placeholder="email" />
-        <input type="password" placeholder="Senha" />
-        <button type="submit">Entrar</button>
+        <Input name="email" icon={FiMail} placeholder="Email" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+        <Button type="submit">Entrar</Button>
         <a href="forgot">Esqueci minha senha</a>
       </form>
       <a href="create">
