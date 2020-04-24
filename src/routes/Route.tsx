6bf5279a60c6436ae2c667f57a687ee38d53c510
Luan Castheloge <luanccp@gmail.com>
2020-04-24 17:57:44 -0300

    chore: security redirect by auth user

---
+++ src/routes/Route.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  Route as ReactDOMRoute,
+  RouteProps as ReactDOMRouteProps,
+  Redirect,
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/auth';
+
+interface RouteProps extends ReactDOMRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType; // chamar o component sem tag
+}
+
+/**   (Rota protegida / user logado)
+ *    true/true = ok
+ *    true/false = redirect login
+ *    false/true = redirect dashboard
+ *    false/false = ok
+ */
+
+const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={({ location }) => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{ pathname: isPrivate ? '/' : 'dashboard', state: location }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
